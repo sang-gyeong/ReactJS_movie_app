08d6d1389cd48ed7454788638d01ff82b170a64b
이상경 <sknglee22@naver.com>
2020-07-14 17:47:21 +0900

    20200714

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function Food({ name, picture, rating }) {
+    return ( < div >
+        <h2> I like { name } </h2>
+        <h4> { rating }/5.0 </h4>
+        <img src = { picture } alt = { name }/>
+        </div>);
+    }
 
-export default App;
+    const foodILike = [{
+            id: 1,
+            name: "Kimchi",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSiXWgh16oNDLdhSZwvLtcJCIEmFZ5utG0hzw&usqp=CAU",
+            rating: 4.8
+        },
+        {
+            id: 2,
+            name: "samgyopsal",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSygiK7oKoRxwNssUNIsV-HeK48MsOi7A6VLg&usqp=CAU",
+            rating: 5.0
+        },
+        {
+            id: 3,
+            name: "Bibimbap",
+            image: "https://recipe1.ezmember.co.kr/cache/recipe/2018/10/03/355b5cd5c3beb1a775c82ee425dcd1931.jpg",
+            rating: 3.4
+        }
+    ]
+
+    Food.proptTypes = {
+
+    }
+
+    function renderFood(dish) {
+        console.log(dish);
+        return <Food key = { dish.id }
+        name = { dish.name }
+        picture = { dish.image }
+        rating = { dish.rating }
+        />;
+    }
+
+    function App() {
+        return ( <
+            div > { foodILike.map(renderFood) } <
+            /div> 
+        );
+    }
+
+    export default App;
