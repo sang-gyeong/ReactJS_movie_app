ee41cd9abf6415ca4fffacea810068fc28dcb293
이상경 <sknglee22@naver.com>
2020-07-17 16:15:21 +0900

    Updates

---
+++ precache-manifest.537a02a2caead9f31d5c3c44ab8b69f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670372a68a6c0ba927a1c5a40ff1e454",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "ebbaabd91db218420806",
+    "url": "/react_movie_app/static/css/main.a89778d2.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/react_movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbaabd91db218420806",
+    "url": "/react_movie_app/static/js/main.711ca7f0.chunk.js"
+  },
+  {
+    "revision": "00ec9c2259bc5a9a77bf",
+    "url": "/react_movie_app/static/js/runtime-main.7e6802fe.js"
+  }
+]);
